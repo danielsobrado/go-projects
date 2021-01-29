b021c448d59fbf8b7ce58bca692c4df26b30c8c6
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2021-01-29 07:43:05 +0400

    exercism

--- webserver/main.go
+++ webserver/main.go
@@ -1,29 +1,20 @@
 package main
 
 import (
-	"crypto/tls"
-	"log"
-	"net"
+	"fmt"
 	"net/http"
-	"time"
 )
 
-type Server struct {
-	Addr           string
-	Handler        Handler
-	ReadTimeout    time.Duration
-	WriteTimeout   time.Duration
-	MaxHeaderBytes int
-	TLSConfig      *tls.Config
-	TLSNextProto   map[string]func(*Server, *tls.Conn, Handler)
-	ConnState      func(net.Conn, ConnState)
-	ErrorLog       *log.Logger
-}
+type MyHandler struct{}
 
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
 func main() {
+	handler := MyHandler{}
 	server := http.Server{
 		Addr:    "127.0.0.1:8080",
-		Handler: nil,
+		Handler: &handler,
 	}
 	server.ListenAndServe()
 }
